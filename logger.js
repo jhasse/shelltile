7ed4ab0d3b08689c6dc22d1dc476219963fe1c62
Emanuele Sabellico <emasab@gmail.com>
2013-04-18 00:32:05 +0200

    fix

--- logger.js
+++ logger.js
@@ -1,33 +1,45 @@
 
-const Logger = function(name){
+const Logger = function(name, level){
     let self = this;
     self.base = name;
-
-    self.log = function(name, message){
-        global.log("[" + name + "]" + self.base + " " + message);
+    if(!level){
+    	level = Logger.DEFAULT_LEVEL;
     }
+    self.level = level;
 
-    self.error = function(message){
-        self.log("ERROR", message);    
-    }
-
-    self.info = function(message){
-        self.log("INFO", message);    
+    self.log = function(name, message, level){
+    	if(level === undefined || self.level === undefined || level >= self.level){
+    		global.log("[" + name + "]" + self.base + " " + message);
+    	}
     }
 
     self.debug = function(message){
-        self.log("DEBUG", message);    
+        self.log("DEBUG", message, Logger.LEVEL_DEBUG);    
+    }
+    
+    self.info = function(message){
+        self.log("INFO", message, Logger.LEVEL_INFO);    
     }
 
     self.warn = function(message){
-        self.log("WARN", message);    
+        self.log("WARN", message, Logger.LEVEL_WARN);    
     }
 
+    self.error = function(message){
+        self.log("ERROR", message, Logger.LEVEL_ERROR);    
+    }    
+    
     self.getLogger = function(clazz){
-        return new Logger(self.base + "[" + clazz + "]");
+        return new Logger(self.base + "[" + clazz + "]", self.level);
     }
 }
 
+Logger.LEVEL_DEBUG = 0;
+Logger.LEVEL_INFO = 1;
+Logger.LEVEL_WARN = 2;
+Logger.LEVEL_ERROR = 3;
+//Logger.DEFAULT_LEVEL = Logger.LEVEL_DEBUG;
+Logger.DEFAULT_LEVEL = Logger.LEVEL_ERROR;
 
 Logger.getLogger = function(module){
     return new Logger("[" + module + "]");
